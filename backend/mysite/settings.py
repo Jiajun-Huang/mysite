c7cc53a02ec29cea4b828fc445adb6a6aff352b1
Jiajun-Huang <jhuang430@qq.com>
2024-09-30 03:00:08 -0700

    sdf

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -167,7 +167,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DEFAULT_FILE_STORAGE = "minio_storage.storage.MinioMediaStorage"
 # STATICFILES_STORAGE = "minio_storage.storage.MinioStaticStorage"
 
-MINIO_STORAGE_URL = urlparse(os.environ.get('MINIO_STORAGE_URL', 'http://192.168.1.10:9000/blog'))
+MINIO_STORAGE_URL = urlparse(os.environ.get('MINIO_STORAGE_URL'))
 MINIO_STORAGE_USE_HTTPS = MINIO_STORAGE_URL.scheme == 'https'
 MINIO_STORAGE_ENDPOINT = MINIO_STORAGE_URL.netloc
 MINIO_STORAGE_ACCESS_KEY = os.environ.get('MINIO_STORAGE_ACCESS_KEY', 'Z2UEZevaUAlmeX3t0W2K')
